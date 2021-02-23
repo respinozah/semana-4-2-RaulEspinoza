89fc1771a890d61030476af8ed11fe3485103a2b
Raul Espinoza <respinozah@gmail.com>
2021-02-23 15:34:17 -0600

    Update Main.java

--- src/com/ucreativa/Main.java
+++ src/com/ucreativa/Main.java
@@ -3,19 +3,28 @@ package com.ucreativa;
 public class Main {
 
 	public static void main(String[] args) {
-		Arbol miArbol = new Arbol("ArbolitusBonitus", "Comediante", 12, "Arbol bonito", "Alto", "Verde");
-		miArbol.ensayar();
-		miArbol.germinar();
+
+		System.out.println("-------------------------------------------------------------------");
+		Profesor miProfe = new Profesor("Profesor X", 48, "X-9383", "profX@mail.com", 12, 8000.0);
+		miProfe.actuar();
+		miProfe.calificarEstudiantexs();
 		
-		Profesor miPersona = new Profesor("Profesor X", 48, "X-9383", "profX@mail.com", 12, 8000.0);
-		miPersona.actuar();
-		miPersona.calificarEstudiantexs();
+		Persona miEstudiante = new Estudiante("Estudiante A", 18, "A-435435", "11", "Ä0-1499", "Tarde");
+		miEstudiante.toString();
+		miEstudiante.ensayar();
 		
+		
+		System.out.println("-------------------------------------------------------------------");
 		Coche miCoche = new Coche("Juan", "Apagado", false, "Burago", "Rojo", "2010");
 		miCoche.arrancar();
 		miCoche.avanzar();
 		miCoche.estarInerte();
 		miCoche.ensayar();
+		
+		System.out.println("-------------------------------------------------------------------");
+		Arbol miArbol = new Arbol("ArbolitusBonitus", "Comediante", 12, "Arbol bonito", "Alto", "Verde");
+		miArbol.ensayar();
+		miArbol.germinar();
 	}
 
 }
